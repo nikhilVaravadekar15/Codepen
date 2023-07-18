a5bb01e9eea23501c96680151f428a9ed168d687
nikhil_varavadekar <nikhil_varavadekar@BHL000190.acds.net.in>
2023-07-18 15:10:21 +0000

    project init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <main className="h-screen w-screen flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold">hello world</h1>
+    </main>
+  )
+}
+
+export default App
